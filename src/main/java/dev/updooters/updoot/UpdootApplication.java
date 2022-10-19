45af6af791aa0558dc0ba44d7f2303c2cd74ea55
smattson613 <smattson613@gmail.com>
2022-10-19 14:45:37 -0400

    Initial commit

---
+++ src/main/java/dev/updooters/updoot/UpdootApplication.java
@@ -0,0 +1,13 @@
+package dev.updooters.updoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/updooters/updoot/UpdootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.updooters.updoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
