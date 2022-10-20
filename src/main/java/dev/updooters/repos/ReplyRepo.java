31c89c0d06ad434d36f881aa055c1de842f0b72c
Timothy Wu <twu23@buffalo.edu>
2022-10-20 12:32:11 -0400

    added essential reply functionality

--- src/main/java/dev/updooters/repos/ReplyRepo.java
+++ src/main/java/dev/updooters/repos/ReplyRepo.java
@@ -4,6 +4,10 @@ import dev.updooters.entities.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReplyRepo extends JpaRepository<Reply, Integer> {
+
+    List<Reply> findBythreadId(int threadId);
 }
--- src/main/java/dev/updooters/updoot/UpdootApplication.java
+++ src/main/java/dev/updooters/updoot/UpdootApplication.java
@@ -2,8 +2,14 @@ package dev.updooters.updoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"dev.updooters"})
+@EntityScan(basePackages = {"dev.updooters.entities"})
+@EnableJpaRepositories(basePackages = {"dev.updooters.repos"})
 public class UpdootApplication {
 
 	public static void main(String[] args) {
