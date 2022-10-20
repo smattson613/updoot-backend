5aa5c035da364019bee09eb2b74abed43b0659f9
Alberto Gutierrez <albertogutierrez700@gmail.com>
2022-10-20 11:47:33 -0400

    get all threads

--- src/main/java/dev/updooters/controllers/ThreadController.java
+++ src/main/java/dev/updooters/controllers/ThreadController.java
@@ -1,9 +1,26 @@
 package dev.updooters.controllers;
 
+import dev.updooters.entities.Thread;
+import dev.updooters.repos.ThreadRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+import java.util.List;
+
+@Controller
 @CrossOrigin
 public class ThreadController {
+
+    @Autowired
+    ThreadRepo threadRepo;
+
+    @GetMapping("/threads")
+    @ResponseBody
+    public List<Thread> getAllThreads(){
+        return this.threadRepo.findAll();
+    }
+
 }
