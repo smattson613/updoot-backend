a7418f9e4cd953f46b5928a1784d9ab99c22bb38
Myles0911 <mdwalker1@aggies.ncat.edu>
2022-10-20 17:42:37 -0400

    basic login functionality added

--- src/main/java/dev/updooters/controllers/AccountController.java
+++ src/main/java/dev/updooters/controllers/AccountController.java
@@ -1,9 +1,23 @@
 package dev.updooters.controllers;
 
+import dev.updooters.dtos.LoginCredentials;
+import dev.updooters.entities.Account;
+import dev.updooters.services.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
 public class AccountController {
+    @Autowired
+    LoginService loginService;
+    @PostMapping("/login")
+    public Account login(@RequestBody LoginCredentials loginCredentials) {
+        Account login = loginService.validateUser(loginCredentials.getUsername(), loginCredentials.getPassword());
+        return login;
+
+    }
 }
--- src/main/java/dev/updooters/repos/AccountRepo.java
+++ src/main/java/dev/updooters/repos/AccountRepo.java
@@ -4,6 +4,11 @@ import dev.updooters.entities.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountRepo extends JpaRepository<Account, Integer> {
+     Account findByUsername(String username);
+
+
 }
