a49f6a45bf6c51ffe3e0f437fd8618014cbf90bf
ontheloosewu <35973933+ontheloosewu@users.noreply.github.com>
2022-10-21 15:16:14 -0400

    Update ThreadController.java

--- src/main/java/dev/updooters/controllers/ThreadController.java
+++ src/main/java/dev/updooters/controllers/ThreadController.java
@@ -4,10 +4,6 @@ import dev.updooters.entities.Thread;
 import dev.updooters.repos.ThreadRepo;
 import dev.updooters.services.ThreadService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
